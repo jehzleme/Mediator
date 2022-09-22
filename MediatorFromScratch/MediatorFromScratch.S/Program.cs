e600e00dbf0077d26d66da7e7104efbf9a0a6419
Jessica Leme Ambrust <jessica.ambrust@ambevtech.com.br>
2022-09-22 08:02:32 -0300

    setting C# version

--- MediatorFromScratch/MediatorFromScratch.S/Program.cs
+++ MediatorFromScratch/MediatorFromScratch.S/Program.cs
@@ -1,23 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MediatorFromScratch.S
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
                 .AddTransient<PrintToConsoleHandler>()
                 .BuildServiceProvider();
+
+            var handlerDetails = new Dictionary<Type, Type>
+            {
+                {typeof(PrintToConsoleRequest), typeof(PrintToConsoleHandler)},
+            };
 
             var request = new PrintToConsoleRequest
             {
                 Text = "Hello from Mediator"
             };
 
-            IMediator mediator = new Mediator(serviceProvider.GetRequiredService);
+            IMediator mediator = new Mediator(serviceProvider.GetRequiredService, handlerDetails);
 
+            await mediator.SendAsync(request);
             //mediator
             //request => mediator => handler => response
         }
--- MediatorFromScratch/MediatorFromScratch/Mediator.cs
+++ MediatorFromScratch/MediatorFromScratch/Mediator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,25 @@ namespace MediatorFromScratch
     public class Mediator : IMediator
     {
         private readonly Func<Type, object> _serviceResolver;
+        private readonly IDictionary<Type, Type> _handlerDetails;
 
-        public Mediator(Func<Type, object> serviceResolver)
+        public Mediator(Func<Type, object> serviceResolver, IDictionary<Type, Type> handlerDetails)
         {
             _serviceResolver = serviceResolver;
+            _handlerDetails = handlerDetails;
         }
 
-        public Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
         {
-            throw new NotImplementedException();
+            var requestType = request.GetType();
+            if (!_handlerDetails.ContainsKey(requestType))
+            throw new Exception($"No handler to handle request of typ: {requestType.Name}");
+
+            _handlerDetails.TryGetValue(requestType, out var requestHandlerType);
+
+            var handler = _serviceResolver(requestHandlerType);
+
+            return handler;
         }
     }
 }
